cabd3894c273132f3fb5f093deb8ce97db8bd880
Mami <nozaru.yeah@gmail.com>
2020-12-19 21:46:31 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: 'home#index'
+  ActiveAdmin.routes(self)
+  # APIコントローラへのルーティング
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :employees, only: [:index, :show]
+    end
+  end
+end
